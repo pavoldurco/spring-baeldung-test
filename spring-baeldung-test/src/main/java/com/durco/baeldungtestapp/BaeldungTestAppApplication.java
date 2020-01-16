b1efb536ab662b67c10e49192bf6673382f13033
Pavol <pavol.durco@centrum.sk>
2020-01-16 12:30:20 +0100

    zalozenie projektu

---
+++ spring-baeldung-test/src/main/java/com/durco/baeldungtestapp/BaeldungTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.durco.baeldungtestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeldungTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeldungTestAppApplication.class, args);
+	}
+
+}
